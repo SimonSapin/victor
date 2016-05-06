58c4c0537a2699b23ac821ff16e559a323af752a
Simon Sapin <simon.sapin@exyr.org>
2016-05-06 23:51:21 +0200

    Take a filename command-line argument.

--- examples/print-paths.rs
+++ examples/print-paths.rs
@@ -1,9 +1,22 @@
 #[macro_use] extern crate string_cache;
 extern crate victor;
 
+use std::path::{Path, PathBuf};
+use std::env;
+
 fn main() {
+    let filename = match env::args().nth(1) {
+        Some(arg) => PathBuf::from(arg),
+        None => Path::new(file!()).parent().unwrap().join("rust-logo").join("rust-logo-blk.svg")
+    };
     let parser = victor::xml::Parser::new();
-    let doc = parser.parse(include_bytes!("rust-logo/rust-logo-blk.svg").as_ref()).unwrap();
+    let doc = match parser.parse_file(filename) {
+        Ok(doc) => doc,
+        Err(error) => {
+            println!("{:?}", error);
+            return
+        }
+    };
     let selector = victor::SelectorList::parse("path[d]").unwrap();
     doc.iter(&mut |node| {
         if let Some(element) = node.as_element() {
