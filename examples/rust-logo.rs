fb25bd2d70d63b53eae93834349cb0e9c6955bcc
Simon Sapin <simon.sapin@exyr.org>
2016-05-06 23:30:26 +0200

    Print all <path> elements, not just the first.

--- examples/rust-logo.rs
+++ examples/rust-logo.rs
@@ -4,14 +4,21 @@ extern crate victor;
 fn main() {
     let parser = victor::xml::Parser::new();
     let doc = parser.parse(include_bytes!("rust-logo/rust-logo-blk.svg").as_ref()).unwrap();
-    let selector = victor::SelectorList::parse("svg > path[d]:first-child").unwrap();
-    let element = selector.query(doc).unwrap();
-    let attribute = element.attribute(&atom!("d")).unwrap();
-    let mut path = victor::svg::path::parse(attribute);
-    for command in &mut path {
-        println!("{:?}", command)
-    }
-    if let Some(error) = path.error() {
-        println!("{:?}", error)
-    }
+    let selector = victor::SelectorList::parse("path[d]").unwrap();
+    doc.iter(&mut |node| {
+        if let Some(element) = node.as_element() {
+            if selector.matches(element) {
+                println!("<path>");
+                let attribute = element.attribute(&atom!("d")).unwrap();
+                let mut path = victor::svg::path::parse(attribute);
+                for command in &mut path {
+                    println!("    {:?}", command)
+                }
+                if let Some(error) = path.error() {
+                    println!("");
+                    println!("    Error around byte {}: {}.", error.position, error.reason);
+                }
+            }
+        }
+    })
 }
