5e147226b2fa404a8994925de191f3fbd710f30f
Simon Sapin <simon.sapin@exyr.org>
2016-05-08 21:56:52 +0200

    Implement center parameterization. Needs tests.

--- src/svg/elliptical_arc.rs
+++ src/svg/elliptical_arc.rs
@@ -1,10 +1,12 @@
 use std::ops::Deref;
-use svg::geometry::{Pair, Matrix2x2, Angle};
+use svg::geometry::{Pair, Matrix2x2, Angle, square};
 use svg::path::SimpleCommand;
 
 /// Together with a starting point (typically the current point of a path),
-/// this provides the *endpoint parameterization* of an elliptical arc.
+/// this provides the *endpoint parameterization* of an elliptical arc,
+/// as it is found in SVG path syntax.
 ///
+/// https://www.w3.org/TR/SVG/paths.html#PathDataEllipticalArcCommands
 /// https://www.w3.org/TR/SVG/implnote.html#ArcSyntax
 #[derive(Copy, Clone, Debug)]
 pub struct ByEndPoint {
@@ -19,6 +21,24 @@ pub struct ByEndPoint {
 /// Provides the *center parameterization* of an elliptical arc.
 ///
 /// https://www.w3.org/TR/SVG/implnote.html#ArcParameterizationAlternatives
+///
+/// Points (x, y) on the arc are:
+///
+/// ```notrust
+/// ( x )   ( cos(φ)  -sin(φ) )   ( radius.x * cos(θ) )   ( center.x )
+/// ( y ) = ( sin(φ)   cos(φ) ) * ( radius.y * sin(θ) ) + ( center.y )
+/// ```
+///
+/// where `θ` goes from `start_angle` to `start_angle + sweep_angle`.
+/// `φ` is `x_axis_rotation`.
+///
+/// `start_angle` and `sweep_angle` are theoretical angles, not measures of the actual arc.
+///
+/// An ellipse can be thought of as a circle that has been stretched along the X or Y axis
+/// by the ratio of `radius.x / radius.y`, then rotated by `x_axis_rotation`.
+/// `θ1` and `Δθ` and `θ + Δθ` are angles in that circle before stretching and rotating.
+///
+/// The arc is clockwise if `sweep_angle` is positive, counter-clockwise if it is negative.
 #[derive(Copy, Clone, Debug)]
 pub struct ByCenter {
     pub center: Pair,
@@ -26,42 +46,88 @@ pub struct ByCenter {
     pub radius: Pair,
     pub x_axis_rotation: Angle,
     pub start_angle: Angle,
-    pub end_angle: Angle,
+    pub sweep_angle: Angle,
 }
 
 impl ByEndPoint {
     /// https://www.w3.org/TR/SVG/implnote.html#ArcConversionEndpointToCenter
-    fn to_center_parameterization(&self, start_point: Pair) {
+    fn to_center_parameterization(&self, start_point: Pair) -> ByCenter {
         let end_point = self.to;
 
         // Steps 2 of https://www.w3.org/TR/SVG/implnote.html#ArcCorrectionOutOfRangeRadii
-        let mut rx = self.radius.x.abs();
-        let mut ry = self.radius.y.abs();
+        let mut radius = self.radius.map(f64::abs);
 
         // Step 1 of https://www.w3.org/TR/SVG/implnote.html#ArcConversionEndpointToCenter
-        let half_middle = (start_point - end_point) / 2.;  // (x1 - x2) / 2
+        let half_diff = (start_point - end_point) / 2.;  // (x1 - x2) / 2
         let cos = self.x_axis_rotation.cos();
         let sin = self.x_axis_rotation.sin();
         let rotation = Matrix2x2(
             cos, sin,
             -sin, cos,
         );
-        let one_prime: Pair = rotation * half_middle;  // (x1', y1')
+        let one_prime = rotation * half_diff;  // (x1', y1')
 
         // Step 3 of https://www.w3.org/TR/SVG/implnote.html#ArcCorrectionOutOfRangeRadii
-        let ratio = Pair { x: one_prime.x / rx, y: one_prime.y / ry };
-        let sum_of_square_ratios = ratio.x * ratio.x + ratio.y * ratio.y;
+        let mut squared_radius = radius.map(square);
+        let squared_one_prime = one_prime.map(square);
+        let sum_of_square_ratios = squared_one_prime.x / squared_radius.x
+                                 + squared_one_prime.y / squared_radius.y;  // Λ
         if sum_of_square_ratios > 1. {
             // The ellipse was not big enough to reach from `start_point` to `end_point`.
-            let root = sum_of_square_ratios.sqrt();
-            rx *= root;
-            ry *= root;
+            radius *= sum_of_square_ratios.sqrt();
+            squared_radius *= sum_of_square_ratios;
         }
 
         // Step 2 of https://www.w3.org/TR/SVG/implnote.html#ArcConversionEndpointToCenter
-        //…
+        let large_numerator =
+              (squared_radius.x * squared_radius.y)
+            - (squared_radius.x * squared_one_prime.y)
+            - (squared_radius.y * squared_one_prime.x);
+        let large_denominator =
+              (squared_radius.x * squared_one_prime.y)
+            + (squared_radius.y + squared_one_prime.x);
+        let sign = if self.large_arc == self.sweep { 1. } else { -1. };
+        let factor = sign * (large_numerator / large_denominator).sqrt();
+        let c_prime = Pair {
+            x:  factor * radius.x * one_prime.y / radius.y,
+            y: -factor * radius.y * one_prime.x / radius.y,
+        };  // (cx', cy')
 
-        unimplemented!();
+        // Step 3 of https://www.w3.org/TR/SVG/implnote.html#ArcConversionEndpointToCenter
+        let rotation = Matrix2x2(
+            cos, -sin,
+            sin, cos,
+        );
+        let center = rotation * c_prime + (start_point + end_point) / 2.;
+
+        // Step 4 of https://www.w3.org/TR/SVG/implnote.html#ArcConversionEndpointToCenter
+        let thing = Pair {
+            x: (one_prime.x - c_prime.x) / radius.x,
+            y: (one_prime.y - c_prime.y) / radius.y,
+        };
+        let other_thing = Pair {
+            x: (-one_prime.x - c_prime.x) / radius.x,
+            y: (-one_prime.y - c_prime.y) / radius.y,
+        };
+        let start_angle = Pair { x: 1., y: 0. }.angle_to(thing);
+        let mut sweep_angle = thing.angle_to(other_thing);
+        if self.sweep {
+            if sweep_angle.as_radians() < 0. {
+                sweep_angle += Angle::from_turns(1.)
+            }
+        } else {
+            if sweep_angle.as_radians() > 0. {
+                sweep_angle -= Angle::from_turns(1.)
+            }
+        }
+
+        ByCenter {
+            center: center,
+            radius: radius,
+            x_axis_rotation: self.x_axis_rotation,
+            start_angle: start_angle,
+            sweep_angle: sweep_angle,
+        }
     }
 
     /// Return a sequence of up to four cubic bezier curve (and in some cases line) commands
@@ -97,7 +163,7 @@ impl ByEndPoint {
             return ArrayPrefix { len: 1, array: [line, dummy, dummy, dummy] }
         }
 
-        let arc = self.to_center_parameterization(start_point);
+        let _arc = self.to_center_parameterization(start_point);
 
         unimplemented!()
     }
--- src/svg/geometry.rs
+++ src/svg/geometry.rs
@@ -1,6 +1,6 @@
 use std::f64::consts::PI;
 use std::fmt;
-use std::ops::{Add, Sub, Mul, Div};
+use std::ops::{Add, AddAssign, Sub, SubAssign, Mul, MulAssign, Div};
 
 #[derive(Copy, Clone, PartialEq)]
 pub struct Pair {
@@ -36,11 +36,44 @@ impl Mul<f64> for Pair {
     }
 }
 
+impl MulAssign<f64> for Pair {
+    fn mul_assign(&mut self, factor: f64) {
+        self.x *= factor;
+        self.y *= factor;
+    }
+}
+
 impl Div<f64> for Pair {
     type Output = Pair;
     fn div(self, factor: f64) -> Pair {
         Pair { x: self.x / factor, y: self.y / factor }
     }
+}
+
+impl Pair {
+    pub fn map<F: Fn(f64) -> f64>(self, f: F) -> Self {
+        Pair {
+            x: f(self.x),
+            y: f(self.y),
+        }
+    }
+
+    pub fn dot_product(self, other: Self) -> f64 {
+        self.x * other.x + self.y * other.y
+    }
+
+    /// Step 4 of https://www.w3.org/TR/SVG/implnote.html#ArcConversionEndpointToCenter
+    pub fn angle_to(self, other: Self) -> Angle {
+        let sign = if self.x * other.y >= self.y * other.x { 1. } else { -1. };
+        let squared_denominator = (square(self.x) + square(self.y))
+                                * (square(other.x) + square(other.y));
+        let ratio = self.dot_product(other) / squared_denominator.sqrt();
+        Angle::from_radians(sign * ratio.acos())
+    }
+}
+
+pub fn square(x: f64) -> f64 {
+    x * x
 }
 
 pub struct Matrix2x2(
@@ -50,9 +83,10 @@ pub struct Matrix2x2(
 
 /// With pairs being "vertical" vectors:
 ///
-///  ( out_x )     ( m0  m1 )     ( x )
-///  (       )  =  (        )  *  (   )
-///  ( out_y )     ( m2  m3 )     ( y )
+/// ```notrust
+/// ( out.x )     ( m.0  m.1 )     ( x )
+/// ( out.y )  =  ( m.2  m.3 )  *  ( y )
+/// ```
 impl Mul<Pair> for Matrix2x2 {
     type Output = Pair;
     fn mul(self, other: Pair) -> Pair {
@@ -73,16 +107,24 @@ impl Angle {
         Angle { radians: radians }
     }
 
+    pub fn from_turns(turns: f64) -> Self {
+        Angle { radians: turns * (2. * PI) }
+    }
+
+    pub fn from_degrees(degrees: f64) -> Self {
+        Self::from_turns(degrees / 360.)
+    }
+
     pub fn as_radians(self) -> f64 {
         self.radians
     }
 
-    pub fn from_degrees(degrees: f64) -> Self {
-        Angle { radians: degrees * PI / 180. }
+    pub fn as_turns(self) -> f64 {
+        self.radians / (2. * PI)
     }
 
     pub fn as_degrees(self) -> f64 {
-        self.radians * 180. / PI
+        self.as_turns() * 360.
     }
 
     pub fn sin(self) -> f64 {
@@ -98,4 +140,30 @@ impl fmt::Debug for Angle {
     fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
          write!(formatter, "{}°", self.as_degrees() as f32)
    }
+}
+
+impl Add for Angle {
+    type Output = Angle;
+    fn add(self, other: Angle) -> Angle {
+        Angle { radians: self.radians + other.radians }
+    }
+}
+
+impl Sub for Angle {
+    type Output = Angle;
+    fn sub(self, other: Angle) -> Angle {
+        Angle { radians: self.radians - other.radians }
+    }
+}
+
+impl AddAssign for Angle {
+    fn add_assign(&mut self, other: Angle) {
+        self.radians += other.radians
+    }
+}
+
+impl SubAssign for Angle {
+    fn sub_assign(&mut self, other: Angle) {
+        self.radians -= other.radians
+    }
 }
