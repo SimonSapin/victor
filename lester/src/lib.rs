ce9e1507f95e78e235354aa26246af0882b1021c
Simon Sapin <simon.sapin@exyr.org>
2017-11-01 16:19:22 +0100

    Link to cairo

--- lester/src/lib.rs
+++ lester/src/lib.rs
@@ -1,6 +1,13 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
+use std::os::raw::c_int;
+
+extern "C" {
+    pub fn cairo_version() -> c_int;
+}
+
+#[test]
+fn it_works() {
+    unsafe {
+        assert!(cairo_version() > 1_12_00);
+        assert!(cairo_version() < 1_17_00);
     }
 }
