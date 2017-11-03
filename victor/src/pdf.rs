5d19d8d3c83aae2669a3b405d10ce5a000d56eba
Simon Sapin <simon.sapin@exyr.org>
2017-11-03 09:33:28 +0100

    Add a dict! macro for constructing lopdf::Dictionary

--- victor/src/pdf.rs
+++ victor/src/pdf.rs
@@ -1,15 +1,25 @@
 use errors::VictorError;
 use display_lists;
 use lopdf::{Document, Object, Dictionary};
-use std::iter::FromIterator;
+
+macro_rules! dict {
+    ($( $key: expr => $value: expr ),+ ,) => {
+        dict!( $($key => $value),+ )
+    };
+    ($( $key: expr => $value: expr ),*) => {{
+        let mut dict = Dictionary::new();
+        $(
+            dict.set($key, $value);
+        )*
+        dict
+    }}
+}
 
 pub(crate) fn from_display_lists(_dl: &display_lists::Document) -> Result<Document, VictorError> {
     let mut doc = Document::with_version("1.5");
-    let catalog_id = doc.add_object(
-        Dictionary::from_iter(vec![
-            ("Type", "Catalog".into()),
-        ])
-    );
+    let catalog_id = doc.add_object(dict!(
+        "Type" => "Catalog",
+    ));
     doc.trailer.set("Root", Object::Reference(catalog_id));
     Ok(doc)
 }
