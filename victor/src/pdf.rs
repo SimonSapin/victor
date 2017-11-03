4640665d7fd283dacb652166e997f98925f12866
Simon Sapin <simon.sapin@exyr.org>
2017-11-04 00:58:22 +0100

    Glob import display_list::* in victor::pdf

--- victor/src/pdf.rs
+++ victor/src/pdf.rs
@@ -1,6 +1,6 @@
 use errors::VictorError;
-use display_lists::{self, DisplayItem, RGB};
-use lopdf::{Document, Object, Stream};
+use display_lists::*;
+use lopdf::{self, Object, Stream};
 use lopdf::content::{Content, Operation};
 
 macro_rules! array {
@@ -18,14 +18,14 @@ const PT_PER_PX: f32 = PT_PER_INCH / PX_PER_INCH;
 const CSS_TO_PDF_SCALE_X: f32 = PT_PER_PX;
 const CSS_TO_PDF_SCALE_Y: f32 = -PT_PER_PX;  // Flip the Y axis direction, it defaults to upwards in PDF.
 
-pub(crate) fn from_display_lists(dl: &display_lists::Document) -> Result<Document, VictorError> {
-    let mut doc = Document::with_version("1.5");
-    let page_tree_id = doc.new_object_id();
+pub(crate) fn from_display_lists(dl: &Document) -> Result<lopdf::Document, VictorError> {
+    let mut pdf_doc = lopdf::Document::with_version("1.5");
+    let page_tree_id = pdf_doc.new_object_id();
 
     let page_ids: Vec<Object> = dl.pages.iter().map(|page| {
         let content = Content { operations: page_content(&page.display_items) };
-        let content_id = doc.add_object(Stream::new(dictionary! {}, content.encode().unwrap()));
-        let page_id = doc.add_object(dictionary! {
+        let content_id = pdf_doc.add_object(Stream::new(dictionary! {}, content.encode().unwrap()));
+        let page_id = pdf_doc.add_object(dictionary! {
             "Type" => "Page",
             "Parent" => page_tree_id,
             "Contents" => content_id,
@@ -39,27 +39,27 @@ pub(crate) fn from_display_lists(dl: &display_lists::Document) -> Result<Documen
         page_id.into()
     }).collect();
 
-    doc.objects.insert(page_tree_id, Object::Dictionary(dictionary! {
+    pdf_doc.objects.insert(page_tree_id, Object::Dictionary(dictionary! {
         "Type" => "Pages",
         "Count" => page_ids.len() as i64,
         "Kids" => page_ids,
     }));
-    let catalog_id = doc.add_object(dictionary!(
+    let catalog_id = pdf_doc.add_object(dictionary!(
         "Type" => "Catalog",
         "Pages" => page_tree_id,
     ));
-    let info_id = doc.add_object(dictionary!(
+    let info_id = pdf_doc.add_object(dictionary!(
         "Producer" => Object::string_literal("Victor <https://github.com/SimonSapin/victor>"),
     ));
 
     // PDF file trailer:
     // https://www.adobe.com/content/dam/acom/en/devnet/pdf/PDF32000_2008.pdf#G6.1941947
-    doc.trailer.set("Root", catalog_id);
-    doc.trailer.set("Info", info_id);
-    Ok(doc)
+    pdf_doc.trailer.set("Root", catalog_id);
+    pdf_doc.trailer.set("Info", info_id);
+    Ok(pdf_doc)
 }
 
-pub fn page_content(display_list: &[display_lists::DisplayItem]) -> Vec<Operation> {
+pub fn page_content(display_list: &[DisplayItem]) -> Vec<Operation> {
     let mut operations = Vec::new();
 
     macro_rules! op {
