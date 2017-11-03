3c69fbf924fc1833326058ba3dfcda0deef494f5
Simon Sapin <simon.sapin@exyr.org>
2017-11-03 10:42:42 +0100

    Victor: create pages! With a size!

--- victor-tests/src/lib.rs
+++ victor-tests/src/lib.rs
@@ -22,5 +22,6 @@ fn pdf() {
     let doc = lester::PdfDocument::from_bytes(&bytes).unwrap();
     assert_eq!(doc.producer().unwrap().to_str().unwrap(),
                "Victor <https://github.com/SimonSapin/victor>");
-    assert_eq!(doc.pages().len(), 0);
+    let sizes: Vec<_> = doc.pages().map(|page| page.size_in_ps_points()).collect();
+    assert_eq!(sizes, [(100., 200.), (300., 400.)])
 }
--- victor/src/pdf.rs
+++ victor/src/pdf.rs
@@ -2,10 +2,32 @@ use errors::VictorError;
 use display_lists;
 use lopdf::{Document, Object};
 
-pub(crate) fn from_display_lists(_dl: &display_lists::Document) -> Result<Document, VictorError> {
+pub(crate) fn from_display_lists(dl: &display_lists::Document) -> Result<Document, VictorError> {
     let mut doc = Document::with_version("1.5");
+    let page_tree_id = doc.new_object_id();
+
+    let page_ids: Vec<Object> = dl.pages.iter().map(|page| {
+        let page_id = doc.add_object(dictionary! {
+            "Type" => "Page",
+            "Parent" => page_tree_id,
+            "MediaBox" => vec![
+                0.into(),
+                0.into(),
+                page.width_in_ps_points.into(),
+                page.height_in_ps_points.into()
+            ],
+        });
+        page_id.into()
+    }).collect();
+
+    doc.objects.insert(page_tree_id, Object::Dictionary(dictionary! {
+        "Type" => "Pages",
+        "Count" => page_ids.len() as i64,
+        "Kids" => page_ids,
+    }));
     let catalog_id = doc.add_object(dictionary!(
         "Type" => "Catalog",
+        "Pages" => page_tree_id,
     ));
     let info_id = doc.add_object(dictionary!(
         "Producer" => Object::string_literal("Victor <https://github.com/SimonSapin/victor>"),
