a7399b52b4c73a3cf9aca56eff67392a0adbef67
Simon Sapin <simon.sapin@exyr.org>
2017-11-05 12:54:33 +0100

    Add a test for scaled rendering.

--- tests/tests.rs
+++ tests/tests.rs
@@ -1,6 +1,7 @@
 #[macro_use] extern crate lester;
 extern crate victor;
 
+use lester::{PdfDocument, RenderOptions};
 use victor::display_lists::*;
 use victor::euclid::rect;
 
@@ -23,14 +24,15 @@ fn pdf() {
     };
     let bytes = dl.write_to_pdf_bytes();
     println!("{}", String::from_utf8_lossy(&bytes));
-    let doc = lester::PdfDocument::from_bytes(&bytes).unwrap();
+    let doc = PdfDocument::from_bytes(&bytes).unwrap();
     assert_eq!(doc.producer().unwrap().to_str().unwrap(),
                "Victor <https://github.com/SimonSapin/victor>");
 
     let sizes: Vec<_> = doc.pages().map(|page| page.size_in_css_px()).collect();
     assert_eq!(sizes, [(20., 10.), (4., 4.)]);
 
-    let mut surface = doc.pages().nth(1).unwrap().render_with_default_options().unwrap();
+    let page = doc.pages().nth(1).unwrap();
+    let mut surface = page.render_with_default_options().unwrap();
     const RED_: u32 = 0x8080_0000;
     const BLUE: u32 = 0xFF00_00FF;
     const BOTH: u32 = 0xFF80_007F;
@@ -41,4 +43,26 @@ fn pdf() {
         BLUE, BLUE, BLUE, BLUE,
         BLUE, BLUE, BLUE, BLUE,
     ]);
+
+    let mut surface = page.render(RenderOptions {
+        dppx_x: 2.0,
+        dppx_y: 3.0,
+        ..RenderOptions::default()
+    }).unwrap();
+    let pixels = surface.pixels();
+    assert_eq!((pixels.width, pixels.height), (8, 12));
+    assert_pixels_eq!(pixels.buffer, &[
+        RED_, RED_, ____, ____, ____, ____, ____, ____,
+        RED_, RED_, ____, ____, ____, ____, ____, ____,
+        RED_, RED_, ____, ____, ____, ____, ____, ____,
+        BOTH, BOTH, BLUE, BLUE, BLUE, BLUE, BLUE, BLUE,
+        BOTH, BOTH, BLUE, BLUE, BLUE, BLUE, BLUE, BLUE,
+        BOTH, BOTH, BLUE, BLUE, BLUE, BLUE, BLUE, BLUE,
+        BLUE, BLUE, BLUE, BLUE, BLUE, BLUE, BLUE, BLUE,
+        BLUE, BLUE, BLUE, BLUE, BLUE, BLUE, BLUE, BLUE,
+        BLUE, BLUE, BLUE, BLUE, BLUE, BLUE, BLUE, BLUE,
+        BLUE, BLUE, BLUE, BLUE, BLUE, BLUE, BLUE, BLUE,
+        BLUE, BLUE, BLUE, BLUE, BLUE, BLUE, BLUE, BLUE,
+        BLUE, BLUE, BLUE, BLUE, BLUE, BLUE, BLUE, BLUE,
+    ][..]);
 }
