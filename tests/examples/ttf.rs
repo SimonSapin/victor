c107f5ef414f0b3938de0b4bb584c50bd684a20c
Simon Sapin <simon.sapin@exyr.org>
2017-11-09 17:29:12 +0100

    Parse 'head' table

--- tests/examples/ttf.rs
+++ tests/examples/ttf.rs
@@ -32,10 +32,16 @@ fn inspect(name: &str, bytes: &[u8]) {
     println!("{} tables: {:?}", table_count, tags);
 
     let table_bytes = |tag: Tag| table_directory_entries().find(|e| e.tag == tag).map(|entry| {
-        &bytes[entry.offset.value() as usize..][..entry.length.value() as usize]
+        &bytes[entry.offset.value() as usize..]
     });
 
-    let info = GeneralInformationTable::cast_from(table_bytes(Tag::from(b"head")).unwrap());
+    let info = GlobalInfo::cast_from(table_bytes(Tag::from(b"head")).unwrap());
+    assert_eq!(info.version.value(), (1, 0));
+    assert_eq!(info.magic_number.value(), 0x5F0F3CF5);
+    println!("crated: {}", info.created.approximate_year());
+    println!("modified: {}", info.modified.approximate_year());
+    println!("bounding box: {:?}", [(info.min_x.value(), info.min_y.value()),
+                                    (info.max_x.value(), info.max_y.value())]);
 }
 
 /// Plain old data: all bit patterns represent valid values
@@ -52,6 +58,7 @@ unsafe trait Pod: Sized {
 
 unsafe impl Pod for u8 {}
 unsafe impl Pod for u16 {}
+unsafe impl Pod for i16 {}
 unsafe impl Pod for u32 {}
 unsafe impl<T: Pod> Pod for [T; 4] {}
 
@@ -77,7 +84,10 @@ macro_rules! big_endian_int_wrappers {
 big_endian_int_wrappers! {
     u32_be: u32;
     u16_be: u16;
+    i16_be: i16;
 }
+
+type FWord = i16_be;
 
 #[repr(C)]
 #[derive(Pod)]
@@ -87,6 +97,40 @@ struct OffsetSubtable {
     search_range: u16_be,
     entry_selector: u16_be,
     range_shift: u16_be,
+}
+
+/// `Fixed` in https://developer.apple.com/fonts/TrueType-Reference-Manual/RM06/Chap6.html
+#[repr(C)]
+#[derive(Pod)]
+struct FixedPoint {
+    integral: u16_be,
+    fractional: u16_be,
+}
+
+impl FixedPoint {
+    fn value(&self) -> (u16, u16) {
+        (self.integral.value(), self.fractional.value())
+    }
+}
+
+#[repr(C)]
+#[derive(Pod)]
+struct LongDateTime {
+    // These two field represent a single 64-bit integer.
+    // We don’t use u64 because TrueType only requires 4-byte alignment.
+    upper_bits: u32_be,
+    lower_bits: u32_be,
+}
+
+impl LongDateTime {
+    fn seconds_since_1904_01_01_midnight(&self) -> u64 {
+        (self.upper_bits.value() as u64) << 32 |
+        self.lower_bits.value() as u64
+    }
+
+    fn approximate_year(&self) -> u64 {
+        (self.seconds_since_1904_01_01_midnight() / 60 / 60 / 24 / 365) + 1904
+    }
 }
 
 #[derive(Pod, Copy, Clone, PartialEq, Eq)]
@@ -104,7 +148,24 @@ struct TableDirectoryEntry {
 
 #[repr(C)]
 #[derive(Pod)]
-struct GeneralInformationTable {
+struct GlobalInfo {
+    version: FixedPoint,
+    font_revision: FixedPoint,
+    checksum_adjustment: u32_be,
+    magic_number: u32_be,
+    flags: u16_be,
+    units_per_em: u16_be,
+    created: LongDateTime,
+    modified: LongDateTime,
+    min_x: FWord,
+    min_y: FWord,
+    max_x: FWord,
+    max_y: FWord,
+    mac_style: u16_be,
+    smallest_readable_size_in_px_per_em: u16_be,
+    font_direction_hint: i16_be,
+    index_to_loc_format: i16_be,
+    glyph_data_format: i16_be,
 }
 
 impl fmt::Debug for Tag {
