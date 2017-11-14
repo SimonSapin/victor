3ac5bfbda2214761e8c2ea10b3afd1261755cb80
Simon Sapin <simon.sapin@exyr.org>
2017-11-14 14:02:43 +0100

    Add some spec links

--- victor/src/lib.rs
+++ victor/src/lib.rs
@@ -9,3 +9,32 @@ extern crate parking_lot_core;
 #[macro_use] extern crate victor_internal_derive;
 
 mod raw_mutex;
+
+/*
+
+## Specifications
+
+PDF:
+    https://www.adobe.com/devnet/pdf/pdf_reference.html
+    https://www.adobe.com/content/dam/acom/en/devnet/pdf/PDF32000_2008.pdf
+
+TrueType:
+    https://developer.apple.com/fonts/TrueType-Reference-Manual/RM06/Chap6.html
+
+OpenType (including TrueType):
+    https://www.microsoft.com/typography/otspec/
+
+PNG:
+    https://www.w3.org/TR/2003/REC-PNG-20031110/
+
+JPEG:
+    https://www.w3.org/Graphics/JPEG/
+
+
+## Font libraries
+
+https://github.com/devongovett/fontkit
+https://github.com/fonttools/fonttools
+https://github.com/bodoni/opentype + https://github.com/bodoni/truetype
+
+*/
