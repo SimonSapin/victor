a5b030f1e6218cea64537bdc1d91498b78764929
Simon Sapin <simon.sapin@exyr.org>
2018-02-25 16:35:52 +0100

    Use a PHF map of parsing function for properties

--- victor/src/style/properties.rs
+++ victor/src/style/properties.rs
@@ -1,9 +1,27 @@
 use cssparser::{Parser, ParseError, CowRcStr, AtRuleParser, DeclarationParser};
-use style::errors::PropertyParseErrorKind;
+use style::errors::{PropertyParseError, PropertyParseErrorKind};
 use style::values::Length;
 
 pub enum PropertyDeclaration {
     FontSize(Length)
+}
+
+type FnParseProperty =
+    for<'i, 't>
+    fn(&mut Parser<'i, 't>)
+    -> Result<PropertyDeclaration, PropertyParseError<'i>>;
+
+ascii_case_insensitive_phf_map! {
+    declaration_parsing_function_by_name -> FnParseProperty = {
+        "font-size" => {
+            // Using a constant works around a spurious borrow-checking error
+            // that I did not bother filing because it is fixed by MIR-based borrow-checking,
+            // so it’ll go away soon enough.
+            // FIXME: remove the indirection when NLL ships.
+            const PARSE: FnParseProperty = |parser| Length::parse(parser).map(PropertyDeclaration::FontSize);
+            PARSE
+        },
+    }
 }
 
 pub struct PropertyDeclarationParser;
@@ -15,11 +33,11 @@ impl<'i> DeclarationParser<'i> for PropertyDeclarationParser {
     fn parse_value<'t>(&mut self, name: CowRcStr<'i>, parser: &mut Parser<'i, 't>)
                        -> Result<Self::Declaration, ParseError<'i, Self::Error>>
     {
-        match_ignore_ascii_case! { &name,
-            "font-size" => return Ok(PropertyDeclaration::FontSize(Length::parse(parser)?)),
-            _ => {}
+        if let Some(parse) = declaration_parsing_function_by_name(&name) {
+            parse(parser)
+        } else {
+            Err(parser.new_custom_error(PropertyParseErrorKind::UnknownProperty(name)))
         }
-        Err(parser.new_custom_error(PropertyParseErrorKind::UnknownProperty(name)))
     }
 }
 
