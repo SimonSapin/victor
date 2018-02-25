63e08b85d693cdb1b04f4d4d02ef00211b230ada
Simon Sapin <simon.sapin@exyr.org>
2018-02-25 16:47:18 +0100

    Macroize property definitions

--- victor/src/style/properties.rs
+++ victor/src/style/properties.rs
@@ -2,24 +2,40 @@ use cssparser::Parser;
 use style::errors::PropertyParseError;
 use style::values::{Parse, Length};
 
-pub enum PropertyDeclaration {
-    FontSize(Length)
-}
-
 type FnParseProperty =
     for<'i, 't>
     fn(&mut Parser<'i, 't>)
     -> Result<PropertyDeclaration, PropertyParseError<'i>>;
 
-ascii_case_insensitive_phf_map! {
-    declaration_parsing_function_by_name -> FnParseProperty = {
-        "font-size" => {
-            // Using a constant works around a spurious borrow-checking error
-            // that I did not bother filing because it is fixed by MIR-based borrow-checking,
-            // so it’ll go away soon enough.
-            // FIXME: remove the indirection when NLL ships.
-            const PARSE: FnParseProperty = |parser| Length::parse(parser).map(PropertyDeclaration::FontSize);
-            PARSE
-        },
+macro_rules! properties {
+    (
+        $( $Variant: ident($ValueType: ty) = $name: expr; )+
+    ) => {
+        pub enum PropertyDeclaration {
+            $(
+                $Variant($ValueType),
+            )+
+        }
+
+        ascii_case_insensitive_phf_map! {
+            declaration_parsing_function_by_name -> FnParseProperty = {
+                $(
+                    $name => {
+                        // Using a constant works around a spurious borrow-checking error
+                        // that I did not bother filing because it is fixed
+                        // by MIR-based borrow-checking, so it’ll go away soon enough.
+                        // FIXME: remove the indirection when NLL ships.
+                        const PARSE: FnParseProperty = |parser| {
+                            <$ValueType as Parse>::parse(parser).map(PropertyDeclaration::$Variant)
+                        };
+                        PARSE
+                    },
+                )+
+            }
+        }
     }
+}
+
+properties! {
+    FontSize(Length) = "font-size";
 }
