9d08d7f9dca3e7ef227c9d4ded25fcd5ef764910
Simon Sapin <simon.sapin@exyr.org>
2018-12-31 10:50:11 +0100

    if let

--- proc-macros/src/generic.rs
+++ proc-macros/src/generic.rs
@@ -2,28 +2,30 @@
 pub fn derive_from_variants(input: TokenStream) -> TokenStream {
     let input: syn::DeriveInput = syn::parse(input).unwrap();
     let name = input.ident;
-
     let mut impls = quote!();
-    match input.data {
-        syn::Data::Enum(data) => {
-            for variant in &data.variants {
-                match variant.fields {
-                    syn::Fields::Unnamed(_) if variant.fields.iter().len() == 1 => {
-                        let variant_name = &variant.ident;
-                        let ty = &variant.fields.iter().next().unwrap().ty;
-                        impls.extend(quote! {
-                            impl std::convert::From<#ty> for #name {
-                                fn from(x: #ty) -> Self {
-                                    #name :: #variant_name (x)
-                                }
-                            }
-                        })
+    if let syn::Data::Enum(data) = input.data {
+        for variant in &data.variants {
+            let variant_name = &variant.ident;
+            let mut fields = variant.fields.iter();
+            if let (syn::Fields::Unnamed(_), Some(field), None) =
+                (&variant.fields, fields.next(), fields.next())
+            {
+                let ty = &field.ty;
+                impls.extend(quote! {
+                    impl std::convert::From<#ty> for #name {
+                        fn from(x: #ty) -> Self {
+                            #name :: #variant_name (x)
+                        }
                     }
-                    _ => {},
-                }
+                })
             }
         }
-        _ => unimplemented!(),
-    };
+    }
+    if impls.is_empty() {
+        panic!(
+            "derive(FromVariants) requires an enum with (some) variants \
+             that have a single unnamed field"
+        )
+    }
     impls.into()
 }
